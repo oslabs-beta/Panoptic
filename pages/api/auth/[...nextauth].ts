6c81be2ca69079c976170e246d5ee1a1c2b41beb
CodedMarc <doramm4408@gmail.com>
2022-06-14 13:03:18 -0500

    github oauth

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,136 @@
+// import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+
+import { NextApiRequest, NextApiResponse } from "next";
+import NextAuth from "next-auth"; //, { InitOptions }
+import Providers from "next-auth/providers";
+// NEXTAUTH_URL='http://localhost:8080'
+
+const options: any = {
+  // Configure one or more authentication providers
+  providers: [    
+        GithubProvider({
+          clientId: '26a39c74c6cc84f5a4ab',
+          clientSecret: '34bf7cb06ce444f2f964633472c538eddef13e26',
+          // https://docs.github.com/en/developers/apps/building-oauth-apps/scopes-for-oauth-apps
+          // @ts-ignore
+          authorization: "https://github.com/login/oauth/authorize?scope=read:user+user:email+repo+read:repo_hook",
+          
+        //   : ["read:user%20repo:status%20read:repo_hook"],
+        }),
+  ]
+};
+
+const login = (req: NextApiRequest, res: NextApiResponse) => {
+    console.log("hi")
+    return NextAuth(req, res, options);
+}
+    
+export default login;
+
+
+
+
+
+// For more information on each option (and a full list of options) go to
+// https://next-auth.js.org/configuration/options
+// export default NextAuth({
+//   // https://next-auth.js.org/configuration/providers
+//   providers: [
+//     // EmailProvider({
+//     //   server: process.env.EMAIL_SERVER,
+//     //   from: process.env.EMAIL_FROM,
+//     // }),
+//     // AppleProvider({
+//     //   clientId: process.env.APPLE_ID,
+//     //   clientSecret: {
+//     //     appleId: process.env.APPLE_ID,
+//     //     teamId: process.env.APPLE_TEAM_ID,
+//     //     privateKey: process.env.APPLE_PRIVATE_KEY,
+//     //     keyId: process.env.APPLE_KEY_ID,
+//     //   },
+//     // }),
+
+
+//     GithubProvider({
+//       clientId: process.env.GITHUB_ID,
+//       clientSecret: process.env.GITHUB_SECRET,
+//       // https://docs.github.com/en/developers/apps/building-oauth-apps/scopes-for-oauth-apps
+//       // @ts-ignore
+//       scope: "read:user",
+//     }),
+
+//   ],
+//   // Database optional. MySQL, Maria DB, Postgres and MongoDB are supported.
+//   // https://next-auth.js.org/configuration/databases
+//   //
+//   // Notes:
+//   // * You must install an appropriate node_module for your database
+//   // * The Email provider requires a database (OAuth providers do not)
+//   // database: process.env.DATABASE_URL,
+
+//   // The secret should be set to a reasonably long random string.
+//   // It is used to sign cookies and to sign and encrypt JSON Web Tokens, unless
+//   // a separate secret is defined explicitly for encrypting the JWT.
+//   secret: process.env.SECRET,
+
+//   session: {
+//     // Use JSON Web Tokens for session instead of database sessions.
+//     // This option can be used with or without a database for users/accounts.
+//     // Note: `strategy` should be set to 'jwt' if no database is used.
+//     strategy: 'jwt'
+
+//     // Seconds - How long until an idle session expires and is no longer valid.
+//     // maxAge: 30 * 24 * 60 * 60, // 30 days
+
+//     // Seconds - Throttle how frequently to write to database to extend a session.
+//     // Use it to limit write operations. Set to 0 to always update the database.
+//     // Note: This option is ignored if using JSON Web Tokens
+//     // updateAge: 24 * 60 * 60, // 24 hours
+//   },
+
+//   // JSON Web tokens are only used for sessions if the `strategy: 'jwt'` session
+//   // option is set - or by default if no database is specified.
+//   // https://next-auth.js.org/configuration/options#jwt
+//   jwt: {
+//     // A secret to use for key generation (you should set this explicitly)
+//     secret: process.env.SECRET,
+//     // Set to true to use encryption (default: false)
+//     // encryption: true,
+//     // You can define your own encode/decode functions for signing and encryption
+//     // if you want to override the default behaviour.
+//     // encode: async ({ secret, token, maxAge }) => {},
+//     // decode: async ({ secret, token, maxAge }) => {},
+//   },
+
+//   // You can define custom pages to override the built-in ones. These will be regular Next.js pages
+//   // so ensure that they are placed outside of the '/api' folder, e.g. signIn: '/auth/mycustom-signin'
+//   // The routes shown here are the default URLs that will be used when a custom
+//   // pages is not specified for that route.
+//   // https://next-auth.js.org/configuration/pages
+//   pages: {
+//     // signIn: '/auth/signin',  // Displays signin buttons
+//     // signOut: '/auth/signout', // Displays form with sign out button
+//     // error: '/auth/error', // Error code passed in query string as ?error=
+//     // verifyRequest: '/auth/verify-request', // Used for check email page
+//     // newUser: null // If set, new users will be directed here on first sign in
+//   },
+
+//   // Callbacks are asynchronous functions you can use to control what happens
+//   // when an action is performed.
+//   // https://next-auth.js.org/configuration/callbacks
+//   callbacks: {
+//     // async signIn({ user, account, profile, email, credentials }) { return true },
+//     // async redirect({ url, baseUrl }) { return baseUrl },
+//     // async session({ session, token, user }) { return session },
+//     // async jwt({ token, user, account, profile, isNewUser }) { return token }
+//   },
+
+//   // Events are useful for logging
+//   // https://next-auth.js.org/configuration/events
+//   events: {},
+
+//   // Enable debug messages in the console if you are having problems
+//   debug: false,
+// })
+
