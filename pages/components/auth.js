43782492b6a7c3bca5fad3ede40509ff52ca4197
Austin Johnson <austinlovsworking@gmail.com>
2022-07-06 13:26:08 -0600

    added sessions

---
+++ pages/components/auth.js
@@ -0,0 +1,25 @@
+
+import { useRouter } from 'next/router'
+
+const Auth = async (userID) => {
+  const router = useRouter();
+
+  const body = JSON.stringify({ userID: userID });
+  const response = await fetch('http://localhost:3000/api/validUser', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: body,
+  })
+    .then((response) => response.json());
+  if (response) {
+    try {
+      return router.push('/dashboard');
+    } catch (err) {
+    }
+  } else {
+    return (false);
+  }
+}
+export default Auth;
--- pages/login.tsx
+++ pages/login.tsx
@@ -4,51 +4,65 @@ import styles from '../styles/Login.module.scss';
 import Nav from './components/Nav';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import Auth from './components/auth';
+import { parseCookies } from '../lib/parseCookies';
 
 // Login page serving file
-function LoginPage(): JSX.Element {
+function LoginPage({ initialRememberValue }): JSX.Element {
+  console.log('COOKIES = ', initialRememberValue);
+
   const githubIcon: JSX.Element = <FaGithub className={styles.githubLogin} />;
   const { data: session, status } = useSession();
-  return (
-    <div className={styles.body}>
-      <Nav />
-      <form
-        className={styles.loginForm}
-        action='/api/login'
-        method='post'
-        id='login'
-      >
-        <input
-          id='username'
-          name='username'
-          placeholder='username'
-          className={styles.username}
-        ></input>
-        <input
-          id='password'
-          name='password'
-          placeholder='password'
-          type='text'
-          className={styles.password}
-        ></input>
-        <button
-          id='loginBtn'
-          type='submit'
-          value='send'
-          className={styles.loginButton}
+  if (!initialRememberValue || !Auth(initialRememberValue)) {
+    // no cookie found for login
+    return (
+      <div className={styles.body}>
+        <Nav />
+        <form
+          className={styles.loginForm}
+          action='/api/login'
+          method='post'
+          id='login'
         >
-          Login
-        </button>
-        <a
-          onClick={() => signIn('github')}
-          className={styles.oauthBtn}
-          href='http://localhost:3000/api/githublogin'
-        >
-          {githubIcon}Sign in with Github
-        </a>
-      </form>
-    </div>
-  );
+          <input
+            id='username'
+            name='username'
+            placeholder='username'
+            className={styles.username}
+          ></input>
+          <input
+            id='password'
+            name='password'
+            placeholder='password'
+            type='text'
+            className={styles.password}
+          ></input>
+          <button
+            id='loginBtn'
+            type='submit'
+            value='send'
+            className={styles.loginButton}
+          >
+            Login
+          </button>
+          <a
+            onClick={() => signIn('github')}
+            className={styles.oauthBtn}
+            href='http://localhost:3000/api/githublogin'
+          >
+            {githubIcon}Sign in with Github
+          </a>
+        </form>
+      </div>
+    );
+  }
+}
+LoginPage.getInitialProps = async ({ req }) => {
+  // Parseing cookie with our own function so we can read it
+  const cookies = parseCookies(req);
+  // Return our cookie and grab name from cookie
+  return {
+    initialRememberValue: cookies.userId,
+  };
 };
-
 export default LoginPage;
